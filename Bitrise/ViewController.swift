90bd253dafec165ab05496b3c5d7471ff5ca60ff
manoj bojja <manojbojja@MacBook-Pro.local>
2017-08-18 10:54:19 +0530

    initial commit

---
+++ Bitrise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bitrise
+//
+//  Created by manoj bojja on 18/08/17.
+//  Copyright © 2017 manoj bojja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
